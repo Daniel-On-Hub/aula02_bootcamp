ae991cab5d430b5a67fcce3125a9e3bb00e453cb
Daniel Lima <daniellima01@gmail.com>
2024-10-10 09:28:47 -0300

    Adicionando aula 14

--- exercicios.py
+++ exercicios.py
@@ -14,3 +14,14 @@ raio_do_circulo = float(input("Digite o raio: "))
 area_do_circulo = math.pi * raio_do_circulo ** 2
 print(f"{area_do_circulo:.2f}")
 
+# Faça um programa que peça o usuário para digitar uma data em formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano.
+
+data_do_usuario = input("Insira uma data no formato dd/mm/aaaa")
+lista_de_dia_mes_ano = data_do_usuario.split("/")
+print(f"O elemento 1 e o: {lista_de_dia_mes_ano[0]}")
+print(f"O elemento 2 e o: {lista_de_dia_mes_ano[1]}")
+print(f"O elemento 3 e o: {lista_de_dia_mes_ano[2]}")
+
+
+
+
